f23866db01d9331507d31719d5a31dbc7206faf8
wasiahmad <wasiahmad@ucla.edu>
2025-12-04 20:00:16 -0800

    making test_25q2 as default split
    Signed-off-by: wasiahmad <wasiahmad@ucla.edu>

--- nemo_skills/dataset/livecodebench-pro/__init__.py
+++ nemo_skills/dataset/livecodebench-pro/__init__.py
@@ -15,5 +15,5 @@
 # settings that define how evaluation should be done by default (all can be changed from cmdline)
 DATASET_GROUP = "code"
 METRICS_TYPE = "code"
-EVAL_SPLIT = "test_25q3"
+EVAL_SPLIT = "test_25q2"
 GENERATION_ARGS = "++prompt_config=eval/livecodebench/python_codegen ++eval_type=livecodebench_pro"
