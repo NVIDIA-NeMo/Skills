b262889870bb5cdfddb63d42501ca1d1b18c264f
mmkrtchyan <mmkrtchyan@nvidia.com>
2026-01-15 16:05:14 +0400

    computeeval
    Signed-off-by: mmkrtchyan <mmkrtchyan@nvidia.com>

--- nemo_skills/evaluation/evaluator/__init__.py
+++ nemo_skills/evaluation/evaluator/__init__.py
@@ -27,6 +27,7 @@ from nemo_skills.evaluation.evaluator.code import (
     eval_livebench_coding,
     eval_livecodebench_pro,
 )
+from nemo_skills.evaluation.evaluator.compute_eval import ComputeEvalEvaluator
 from nemo_skills.evaluation.evaluator.icpc import ICPCEvaluator
 from nemo_skills.evaluation.evaluator.ifbench import eval_ifbench
 from nemo_skills.evaluation.evaluator.ifeval import eval_if
@@ -69,7 +70,7 @@ EVALUATOR_CLASS_MAP = {
     "icpc": ICPCEvaluator,
     "audio": AudioEvaluator,
     "bird": BirdEvaluator,
-    "compute-eval": "nemo_skills.evaluation.evaluator.compute_eval:ComputeEvalEvaluator",
+    "compute-eval": ComputeEvalEvaluator,
 }
 
 # Validation: Ensure no overlap between class and function maps
