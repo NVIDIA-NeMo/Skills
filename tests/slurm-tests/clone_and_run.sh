86d9c716f559288948b796bbd976107b5c655d64
Igor Gitman <igitman@nvidia.com>
2026-01-25 21:49:31 -0800

    Fix bfcl check results
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/slurm-tests/clone_and_run.sh
+++ tests/slurm-tests/clone_and_run.sh
@@ -9,8 +9,7 @@ LOCAL_WORKSPACE=/tmp/nemo-skills-slurm-ci
 rm -rf $LOCAL_WORKSPACE
 mkdir -p $LOCAL_WORKSPACE
 cd $LOCAL_WORKSPACE
-# TODO: temporarily copying from local folder for testing, revert to git clone after
-cp -r /home/igitman/workspace/NeMo-Skills NeMo-Skills
+git clone https://github.com/NVIDIA-NeMo/Skills.git NeMo-Skills
 cd NeMo-Skills
 
 curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR=$LOCAL_WORKSPACE sh
