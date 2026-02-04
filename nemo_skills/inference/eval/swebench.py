fbdaf2fffc5a7c18afba24f5fff63bff72a425b4
wasiahmad <wasiahmad@ucla.edu>
2026-02-04 13:19:31 -0800

    updating mini-swe-agent cmd
    Signed-off-by: wasiahmad <wasiahmad@ucla.edu>

--- nemo_skills/inference/eval/swebench.py
+++ nemo_skills/inference/eval/swebench.py
@@ -259,7 +259,7 @@ class SweBenchGenerationTask(GenerationTask):
             if self.cfg.agent_framework_repo is None:
                 self.cfg.agent_framework_repo = "https://github.com/SWE-agent/mini-swe-agent.git"
             if self.cfg.agent_framework_commit is None:
-                self.cfg.agent_framework_commit = "HEAD"
+                self.cfg.agent_framework_commit = "v1"
             setup_commands.append(
                 # clone the swe-agent repo
                 "rm -rf /root/mini-swe-agent && "
