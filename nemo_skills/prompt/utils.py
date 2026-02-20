aa9e7fd3468fd56ae0951f3b65758f6a7567ccf0
Igor Gitman <igitman@nvidia.com>
2026-02-20 14:13:33 -0800

    Debugging
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/prompt/utils.py
+++ nemo_skills/prompt/utils.py
@@ -397,8 +397,11 @@ def get_token_count(
         try:
             result = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, tools=tools)
             # Handle newer HF tokenizer versions that return a dict instead of a list
+            print(result, type(result))
             if isinstance(result, dict):
                 result = result["input_ids"]
+                print("!@#")
+            print(len(result))
             return len(result)
 
         except Exception as e:
