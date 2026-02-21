f5986e57b7791a10b459fbab8704c4dea449cbd5
Jiacheng Xu <jcxu@utexas.edu>
2026-02-20 16:42:01 -0800

    Add CritPt benchmark (#1200)
    Signed-off-by: Jiacheng Xu <jiachengx@nvidia.com>
    Co-authored-by: Igor Gitman <igitman@nvidia.com>
    Signed-off-by: suriya <sgunasekar@nvidia.com>

--- nemo_skills/evaluation/evaluator/__init__.py
+++ nemo_skills/evaluation/evaluator/__init__.py
@@ -30,6 +30,7 @@ from nemo_skills.evaluation.evaluator.code import (
     eval_livecodebench_pro,
 )
 from nemo_skills.evaluation.evaluator.compute_eval import ComputeEvalEvaluator
+from nemo_skills.evaluation.evaluator.critpt import CritPtEvaluator
 from nemo_skills.evaluation.evaluator.dsbench import DSBenchEvaluator
 from nemo_skills.evaluation.evaluator.icpc import ICPCEvaluator
 from nemo_skills.evaluation.evaluator.ifbench import eval_ifbench
@@ -75,6 +76,7 @@ EVALUATOR_CLASS_MAP = {
     "audio": AudioEvaluator,
     "bird": BirdEvaluator,
     "compute-eval": ComputeEvalEvaluator,
+    "critpt": CritPtEvaluator,
     "dsbench": DSBenchEvaluator,
 }
 
